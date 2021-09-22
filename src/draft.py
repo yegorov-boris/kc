2d0d65388a3d57893e650998e3618e3217f8cd6c
yegorov-boris <borisyegorov8@gmail.com>
2021-09-22 13:14:51 +0300

    module 1 hw 5: check 5 train took too much time

--- src/draft.py
+++ src/draft.py
@@ -1,3 +1,6 @@
+from datetime import datetime
 from s_t_1_5 import Solution
 s = Solution(glue_qqp_dir='../data/QQP/', glove_vectors_path='../data/glove.6B.50d.txt')
-s.train(8)
+t = datetime.now()
+s.train(1)
+print(datetime.now() - t)
