6784094b4f975b0922953c8f3a23cca934221ac9
yegorov-boris <borisyegorov8@gmail.com>
2021-09-22 17:27:37 +0300

    module 1 hw 6: pass check 1

--- src/draft.py
+++ src/draft.py
@@ -1,3 +1,8 @@
-from s_t_1_5 import Solution
-s = Solution(glue_qqp_dir='../data/QQP/', glove_vectors_path='../data/glove.6B.50d.txt')
-s.train(8)
+import numpy as np
+from s_t_1_6 import create_sw_graph, distance
+
+data = np.array([[1, 9], [2, 3], [4, 1], [3, 7], [5, 4], [6, 8], [7, 2], [8, 8], [7, 9], [9, 6]])
+
+# print(create_sw_graph(data, 4, 2, 4, 2))
+
+print(distance(data[0], data[1:]))
